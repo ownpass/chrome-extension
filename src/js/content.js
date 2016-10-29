4172b37eac0b7f28a73be4a493726726eee2f4c1
Walter Tamboer <walter@tamboer.nl>
2016-10-29 17:44:41 +0200

    Fixed the submitting of the form.

--- src/js/content.js
+++ src/js/content.js
@@ -1,12 +1,33 @@
 (function () {
-    var ownPassTopBar = null;
+    var zIndex = 100000, ownPassBackground = null, ownPassTopBar = null, submitForm = null;
 
     function removeTopBar() {
+        if (ownPassBackground) {
+            document.body.removeChild(ownPassBackground);
+
+            ownPassBackground = null;
+        }
+
         if (ownPassTopBar) {
             document.body.removeChild(ownPassTopBar);
 
             ownPassTopBar = null;
         }
+    }
+
+    function createTransparentBackground() {
+        var backgroundDiv = document.createElement('div');
+
+        backgroundDiv.style.backgroundColor = '#FFF';
+        backgroundDiv.style.bottom = 0;
+        backgroundDiv.style.left = 0;
+        backgroundDiv.style.opacity = '0.8';
+        backgroundDiv.style.position = 'fixed';
+        backgroundDiv.style.top = 0;
+        backgroundDiv.style.right = 0;
+        backgroundDiv.style.zIndex = zIndex - 1;
+
+        return backgroundDiv;
     }
 
     function createButton(title, callback) {
@@ -50,7 +71,7 @@
         topBar.style.position = 'fixed';
         topBar.style.top = 0;
         topBar.style.right = 0;
-        topBar.style.zIndex = 100000;
+        topBar.style.zIndex = zIndex;
 
         topBar.appendChild(spanText);
         topBar.appendChild(createButton('No', noCallback));
@@ -62,24 +83,46 @@
     function onFormSubmit(e) {
         var form = this;
 
+        // When the form has already been checked, allow the submit.
+        if (form.getAttribute('data-ownpass-checked') === 'true') {
+            return true;
+        }
+
         e.preventDefault();
 
         ownPassTopBar = createTopBar(
             document.title,
             function () {
                 removeTopBar();
-
-                form.dispatchEvent(new Event('submit'));
+                submitForm(form);
             },
             function () {
                 removeTopBar();
-
-                form.dispatchEvent(new Event('submit'));
+                submitForm(form);
             }
         );
 
+        ownPassBackground = createTransparentBackground();
+
         document.body.appendChild(ownPassTopBar);
+        document.body.appendChild(ownPassBackground);
+        return false;
     }
+
+    submitForm = function(form) {
+        form.setAttribute('data-ownpass-checked', true);
+
+        if (form.submit instanceof Function) {
+            form.submit();
+        } else {
+            var inputs = document.forms[0].getElementsByTagName('input');
+            for (var i = 0; i < inputs.length; ++i) {
+                if (inputs[i].type === 'submit') {
+                    inputs[i].click();
+                }
+            }
+        }
+    };
 
     document.addEventListener('DOMContentLoaded', function () {
         var forms = document.getElementsByTagName('form');
