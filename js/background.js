dd239b0732697fa5133b666c43abe2f56d206eac
Walter Tamboer <walter@tamboer.nl>
2016-11-07 23:33:03 +0100

    Made it possible to recover access to an account

--- js/background.js
+++ js/background.js
@@ -12,7 +12,7 @@ function sendMessageToContent(msg) {
 }
 
 function Handler() {
-    this.displayReminder = function(msg, sender, callback) {
+    this.displayReminder = function (msg, sender, callback) {
         var notification;
 
         if (remindedUnauthenticated) {
@@ -39,19 +39,27 @@ function Handler() {
     this.hasIdentity = function (msg, sender, callback) {
         var token = JSON.parse(window.localStorage.getItem('oauth-token'));
         var deviceIds = JSON.parse(window.localStorage.getItem('device-ids'));
+        var deviceId, deviceActivated = false;
 
         if (!token) {
             callback({
                 authenticated: false,
-                validDevice: false
+                deviceActivated: false,
+                deviceId: null
             });
 
             return false;
         }
 
+        if (deviceIds && deviceIds[token.username]) {
+            deviceActivated = deviceIds[token.username].activated;
+            deviceId = deviceIds[token.username].id;
+        }
+
         callback({
             authenticated: true,
-            validDevice: deviceIds ? !!deviceIds[token.username] : false
+            deviceActivated: deviceActivated,
+            deviceId: deviceId
         });
 
         return false;
@@ -71,13 +79,13 @@ function Handler() {
                 password: msg.password
             }),
             dataType: 'json',
-            error : function (jqXHR) {
+            error: function (jqXHR) {
                 callback({
                     error: jqXHR.responseJSON.error_description
                 });
             },
-            success: function(data) {
-                var deviceIds, deviceId;
+            success: function (data) {
+                var deviceIds, deviceId, deviceActivated = false;
 
                 data.server = msg.server;
                 data.username = msg.username;
@@ -85,16 +93,21 @@ function Handler() {
                 window.localStorage.setItem('oauth-token', JSON.stringify(data));
 
                 deviceIds = JSON.parse(window.localStorage.getItem('device-ids'));
-                deviceId = deviceIds ? deviceIds[msg.username] : false;
 
-                if (deviceId) {
+                if (deviceIds && deviceIds[msg.username]) {
+                    deviceId = deviceIds[msg.username].id;
+                    deviceActivated = deviceIds[msg.username].activated;
+                }
+
+                if (deviceActivated) {
                     sendMessageToContent({
                         cmd: 'ownpass-activated'
                     });
                 }
 
                 callback({
-                    validDevice: !!deviceId,
+                    deviceActivated: deviceActivated,
+                    deviceId: deviceId,
                     token: data
                 });
             }
@@ -115,9 +128,50 @@ function Handler() {
         return false;
     };
 
-    this.activateDevice = function (msg, sender, callback) {
+    this.forgetDevice = function (msg, sender, callback) {
+        var token = JSON.parse(window.localStorage.getItem('oauth-token'));
+        var deviceIds = JSON.parse(window.localStorage.getItem('device-ids'));
+
+        delete deviceIds[token.username];
+
+        window.localStorage.setItem('device-ids', JSON.stringify(deviceIds));
+
         callback();
-        return false;
+    };
+
+    this.activateDevice = function (msg, sender, callback) {
+        var token = JSON.parse(window.localStorage.getItem('oauth-token'));
+
+        $.ajax({
+            url: token.server + '/device/activate',
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + token.access_token,
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify({
+                code: msg.code
+            }),
+            dataType: 'json',
+            error: function (jqXHR) {
+                callback({
+                    error: jqXHR.responseJSON.detail
+                });
+            },
+            success: function () {
+                var deviceIds = JSON.parse(window.localStorage.getItem('device-ids'));
+
+                deviceIds[token.username].activated = true;
+
+                window.localStorage.setItem('device-ids', JSON.stringify(deviceIds));
+
+                callback({
+                    error: false
+                });
+            }
+        });
+
+        return true;
     };
 
     this.registerDevice = function (msg, sender, callback) {
@@ -136,18 +190,21 @@ function Handler() {
                 description: msg.description
             }),
             dataType: 'json',
-            error : function (jqXHR) {
+            error: function (jqXHR) {
                 callback({
                     status: jqXHR.status,
                     error: jqXHR.responseJSON.error_description
                 });
             },
-            success: function(data) {
+            success: function (data) {
                 if (!deviceIds) {
                     deviceIds = {};
                 }
 
-                deviceIds[token.username] = data.id;
+                deviceIds[token.username] = {
+                    id: data.id,
+                    activated: false
+                };
 
                 window.localStorage.setItem('device-ids', JSON.stringify(deviceIds));
 
@@ -157,6 +214,58 @@ function Handler() {
             }
         });
         return false;
+    };
+
+    this.activateAccount = function (msg, sender, callback) {
+        $.ajax({
+            url: msg.server + '/account/activate',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify({
+                activation_code: msg.code,
+                credential: msg.credential
+            }),
+            dataType: 'json',
+            error: function (jqXHR) {
+                callback({
+                    status: jqXHR.status,
+                    error: jqXHR.responseJSON.detail
+                });
+            },
+            success: function (data) {
+                console.log(data);
+                callback(data);
+            }
+        });
+
+        return true;
+    };
+
+    this.recoverAccount = function (msg, sender, callback) {
+        $.ajax({
+            url: msg.server + '/account/deactivate',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify({
+                email_address: msg.email
+            }),
+            dataType: 'json',
+            error: function (jqXHR) {
+                callback({
+                    status: jqXHR.status,
+                    error: jqXHR.responseJSON.error_description
+                });
+            },
+            success: function (data) {
+                callback(data);
+            }
+        });
+
+        return true;
     };
 }
 
@@ -195,12 +304,24 @@ chrome.runtime.onMessage.addListener(function (msg, sender, callback) {
             result = handler.logout(msg, sender, callback);
             break;
 
+        case 'ownpass-forget-device':
+            result = handler.forgetDevice(msg, sender, callback);
+            break;
+
         case 'ownpass-activate-device':
             result = handler.activateDevice(msg, sender, callback);
             break;
 
         case 'ownpass-register-device':
             result = handler.registerDevice(msg, sender, callback);
+            break;
+
+        case 'ownpass-activate-account':
+            result = handler.activateAccount(msg, sender, callback);
+            break;
+
+        case 'ownpass-recover-account':
+            result = handler.recoverAccount(msg, sender, callback);
             break;
     }
 
--- js/popup.js
+++ js/popup.js
@@ -12,18 +12,39 @@ $(function () {
     var activateScreen = function (name) {
         hideScreens();
 
-        if (name === 'login-screen') {
-            resetLoginScreen();
+        switch (name) {
+            case 'login-screen':
+                resetLoginScreen();
+                break;
+
+            case 'activate-account-screen':
+                $('#activate-account-server').val(window.localStorage.getItem('login-server'));
+                break;
+
+            case 'recover-screen':
+                $('#recover-server').val(window.localStorage.getItem('login-server'));
+                break;
+
+            default:
+                break;
         }
 
         $('#' + name).show();
     };
 
     var bindCallbacks = function () {
-        $('[data-op-goto]').on('click', function (e) {
+        $('.forget-device-button').on('click', function (e) {
             e.preventDefault();
 
-            activateScreen($(this).data('op-goto'));
+            chrome.runtime.sendMessage(null, {
+                'cmd': 'ownpass-forget-device'
+            }, function () {
+                chrome.runtime.sendMessage(null, {
+                    'cmd': 'ownpass-logout'
+                });
+
+                activateScreen('login-screen');
+            });
         });
 
         $('.logout-button').on('click', function (e) {
@@ -34,6 +55,12 @@ $(function () {
             });
 
             activateScreen('login-screen');
+        });
+
+        $('[data-op-goto]').on('click', function (e) {
+            e.preventDefault();
+
+            activateScreen($(this).data('op-goto'));
         });
 
         $('#login-screen form').on('submit', function (e) {
@@ -47,21 +74,21 @@ $(function () {
             $('.op-form-error', this).hide();
 
             if (server === '') {
-                $('#login-server', this).next('.op-form-error').show().html('The server is required.');
+                $('#login-server', this).next('.op-form-error').show().text('The server is required.');
                 return;
             }
 
             window.localStorage.setItem('login-server', server);
 
             if (username === '') {
-                $('#login-username', this).next('.op-form-error').show().html('The username is required.');
+                $('#login-username', this).next('.op-form-error').show().text('The username is required.');
                 return;
             }
 
             window.localStorage.setItem('login-username', username);
 
             if (password === '') {
-                $('#login-password', this).next('.op-form-error').show().html('The password is required.');
+                $('#login-password', this).next('.op-form-error').show().text('The password is required.');
                 return;
             }
 
@@ -72,9 +99,11 @@ $(function () {
                 'password': password
             }, function (response) {
                 if (response.error) {
-                    passwordField.next('.op-form-error').show().html(response.error);
-                } else if (response.validDevice) {
+                    passwordField.next('.op-form-error').show().text(response.error);
+                } else if (response.deviceActivated) {
                     activateScreen('popup-screen');
+                } else if (response.deviceId) {
+                    activateScreen('activate-device-screen');
                 } else {
                     activateScreen('register-device-screen');
                 }
@@ -88,7 +117,7 @@ $(function () {
             e.preventDefault();
 
             if (description === '') {
-                descriptionField.next('.op-form-error').show().html('The description is required.');
+                descriptionField.next('.op-form-error').show().text('The description is required.');
                 return;
             }
 
@@ -100,14 +129,96 @@ $(function () {
             activateScreen('activate-device-screen');
         });
 
+        $('#recover-screen form').on('submit', function (e) {
+            var serverField = $('#recover-server', this), emailField = $('#recover-email', this);
+            var server = serverField.val(), email = emailField.val();
+
+            e.preventDefault();
+
+            $('.op-form-error', this).hide();
+
+            if (server === '') {
+                serverField.next('.op-form-error').show().text('A server is required.');
+                return;
+            }
+
+            if (email === '') {
+                emailField.next('.op-form-error').show().text('Your e-mail address is required.');
+                return;
+            }
+
+            chrome.runtime.sendMessage(null, {
+                'cmd': 'ownpass-recover-account',
+                'server': server,
+                'email': email
+            }, function (response) {
+                if (response.error) {
+                    emailField.next('.op-form-error').show().text(response.error);
+                } else {
+                    activateScreen('activate-account-screen');
+                }
+            });
+        });
+
+        $('#activate-account-screen').on('submit', function (e) {
+            var serverField = $('#activate-account-server', this),
+                server = serverField.val(),
+                codeField = $('#activate-account-code', this),
+                code = codeField.val(),
+                credentialField = $('#activate-account-credential', this),
+                credential = credentialField.val(),
+                validationField = $('#activate-account-validation', this),
+                validation = validationField.val();
+
+            e.preventDefault();
+
+            $('.op-form-error', this).hide();
+
+            if (server === '') {
+                serverField.next('.op-form-error').show().text('The server is required.');
+                return;
+            }
+
+            if (code === '') {
+                codeField.next('.op-form-error').show().text('The code is required.');
+                return;
+            }
+
+            if (credential === '') {
+                credentialField.next('.op-form-error').show().text('The credential is required.');
+                return;
+            }
+
+            if (credential !== validation) {
+                validationField.next('.op-form-error').show().text('The two passwords do not match.');
+                return;
+            }
+
+            chrome.runtime.sendMessage(null, {
+                'cmd': 'ownpass-activate-account',
+                'server': server,
+                'code': code,
+                'credential': credential
+            }, function (response) {
+                console.log(response);
+                if (response.error) {
+                    validationField.next('.op-form-error').show().text(response.error);
+                } else {
+                    activateScreen('login-screen');
+                }
+            });
+        });
+
         $('#activate-device-screen form').on('submit', function (e) {
             var codeField = $('#activate-device-code', this);
             var code = codeField.val();
 
             e.preventDefault();
 
+            $('.op-form-error', this).hide();
+
             if (code === '') {
-                codeField.next('.op-form-error').show().html('The code is required.');
+                codeField.next('.op-form-error').show().text('The code is required.');
                 return;
             }
 
@@ -115,8 +226,8 @@ $(function () {
                 'cmd': 'ownpass-activate-device',
                 'code': code
             }, function (response) {
-                if (response.success) {
-
+                if (response.error) {
+                    codeField.next('.op-form-error').show().text(response.error);
                 } else {
                     activateScreen('popup-screen');
                 }
@@ -138,8 +249,10 @@ $(function () {
     }, function (response) {
         console.log(response);
 
-        if (response.validDevice) {
+        if (response.deviceActivated) {
             activateScreen('popup-screen');
+        } else if (response.deviceId) {
+            activateScreen('activate-device-screen');
         } else if (response.authenticated) {
             activateScreen('register-device-screen');
         } else {
